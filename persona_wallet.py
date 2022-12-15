ec4970d33901ef8bf810f62fed6daf3412e9bbdd
Mike <mikezhao980306@gmail.com>
2022-12-14 16:03:51 -0800

    added logic to add peers to wallet. added logic to create secret file if not existed. added path for file which is under secret folder

--- persona_wallet.py
+++ persona_wallet.py
@@ -1,7 +1,8 @@
 from meshtastic import persona_pb2
 from nacl.signing import SigningKey
+import BlackLagerMessage_pb2
 import pickle
-import sys
+import sys, os
 
 
 class PersonaWallet:
@@ -11,10 +12,10 @@ class PersonaWallet:
         """Initialize a Wallet object. Check if a command line argument is provided for the wallet file.
         If there is one provided, then set it as the wallet path. Else set the default wallet path
         """
-        self.wallet_message = persona_pb2.Wallet()
+        self.wallet_message = BlackLagerMessage_pb2.Wallet()
 
         if len(sys.argv) == 1:
-            self.wallet_path = "./wallet"
+            self.wallet_path = self.get_config_path()
         elif len(sys.argv) == 2:
             self.wallet_path = sys.argv[1]
         else:
@@ -43,6 +44,19 @@ class PersonaWallet:
 
             self.current_persona = self.wallet_message.my_personas[persona_selection]
 
+
+    def get_config_path(self):
+        path_to_script = os.path.dirname(os.path.realpath(__file__))
+        return path_to_script[:-13] + "/secret/config.txt"
+
+    def create_local_file(self):
+        if os.path.exists(self.wallet_path):
+            return
+        else:
+            #Create a new file
+            with open(self.wallet_path, 'w') as fp:
+                pass
+
     def read_wallet_from_file(self):
         """Read wallet data from the wallet_path file and parse it into the wallet_message protobuf object"""
         try:
@@ -62,12 +76,16 @@ class PersonaWallet:
         new_persona.public_key = pickle.dumps(signing_key.verify_key)
         return new_persona
 
-    def save_peer_persona(self, name, public_key):
+    def save_peer_persona(self, node_num, public_key, friendly_name):
         """Save a peer persona to wallet"""
-        peer_persona = self.wallet_message.peer_personas.add()
-        peer_persona.owned = False
-        peer_persona.local_name = name
-        peer_persona.public_key = public_key
+        self.configure_peer_persona(self.wallet_message.peer_personas.add(), node_num, public_key, friendly_name )
+        self.write_wallet_to_file()
+
+    def configure_peer_persona(self, persona, node_num, public_key, friendly_name):
+        persona.local_name = friendly_name
+        persona.public_key = public_key
+        persona.owned = False
+        persona.node_num = node_num
 
     def write_wallet_to_file(self):
         """Writes wallet data out to a file on disk"""
